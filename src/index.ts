1e9431274dd072f4c6c5216ebbd9b5377d3ec039
Igor.tdf <104040266+IgorTDF@users.noreply.github.com>
2022-05-10 19:21:34 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+//let arregloMes : string[] = new Array(12);
+
+//let num: number[] = new Array(5);
+//let indice: number = Number ( prompt ("ingresar un numero"));//
+
+/*for (let i: number = 0; i < 5; i++) {
+  num[i] = Number(prompt("ingresar un numero"));
+
+  console.log(" el numero ingresado es: ", num[i]);
+}*/
+
+//console.log(" los numeros ingresados son ", mes);
+
+/*•Construya un algoritmo que tenga un arreglo
+de números y se los muestre al usuario*/
+//•El arreglo debe ser llamado num
+/*•El arreglo num debe contener los siguientes
+datos: 20, 14, 8, 0, 5, 19 y 24.*/
+let Num: number[] = [20, 14, 8, 0, 5, 19, 24];
+
+for (let i: number = 0; i <= 7; i++) {
+  console.log(Num[i - 1]);
+}
+
+/*• Mostrar los valores resultantes del arreglo*/
